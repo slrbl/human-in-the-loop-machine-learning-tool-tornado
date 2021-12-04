72695487ef71a7bfbab78d2732cc005c76c18c84
slrbl <walid.daboubi@gmail.com>
2021-12-04 22:55:01 +0100

    useless migration removal

--- db/migrate/20210417232903_add_humanlabel_count_training2.rb
+++
@@ -1,6 +0,0 @@
-class AddHumanlabelCountTraining2 < ActiveRecord::Migration[5.1]
-  def change
-    add_column :trainings, :humna_label_count, :integer
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210417232903) do
+ActiveRecord::Schema.define(version: 20210417232022) do
 
   create_table "datasets", force: :cascade do |t|
     t.string "name"
