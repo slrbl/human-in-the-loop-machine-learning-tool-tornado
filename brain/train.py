48c0d2644c0d1b83c3e2ab231bdeb3588748c4ce
Walid Daboubi <wdaboubi@Walids-MacBook-Pro.local>
2022-03-13 22:23:41 +0100

    refactoring brain/train.py

--- brain/train.py
+++ brain/train.py
@@ -17,120 +17,80 @@ parser.add_argument('-l', '--label', help="The label attribute")
 parser.add_argument('-i', '--es_id', help="Elasticseach data id")
 parser.add_argument('-s', '--es_address', help="Elasticseach data id")
 
+# getting arguments
 args = parser.parse_args()
-
-print args.features
-print args.label
-print args.es_id
-
 features = args.features
 label = args.label
 es_id = args.es_id
 es_address = args.es_address
 
+# Constants
+HEADERS = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+
 source = features.split(',')
 source.append(label)
 
 
-URL = es_address + '/_search/'
+es_url = '{}/_search'.format(es_address)
 query = {'query':{'bool': {'must': [{'match': {'es_id': es_id}}, {'match': {'es_id': es_id}}]}}, 'size': 10000, '_source': source}
-headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-
-response = requests.get(URL, data = json.dumps(query), headers = headers)
-result =  json.loads(response.text)
+response = requests.get(es_url, data = json.dumps(query), headers = HEADERS)
+data_result =  json.loads(response.text)
 
 # Vectorization
 vectors = {}
 for feature in features.split(','):
     vectors[feature] = []
+vectors[label] = []
 
 
-vectors[label]=[]
-print vectors
-
-for unit in result['hits']['hits']:
+for unit in data_result['hits']['hits']:
     for feature in unit['_source']:
-        print type(unit['_source'][feature])
-        if feature!=label and unit['_source'][feature] not in vectors[feature]:
+        print (type(unit['_source'][feature]))
+        if feature != label and unit['_source'][feature] not in vectors[feature]:
             vectors[feature].append(unit['_source'][feature])
-    if unit['_source'][label]!='empty' and unit['_source'][label] not in vectors[label]:vectors[label].append(unit['_source'][label])
+    if unit['_source'][label] != 'empty' and unit['_source'][label] not in vectors[label]:vectors[label].append(unit['_source'][label])
 
-for vector in vectors:
-    print vector
-    print vectors[vector]
-    print '------'
 
-#   print result
-
+# X,y initialization
 X = []
 y = []
 
-
-
-print label
-for unit in result['hits']['hits']:
-    if unit['_source'][label]!='empty':
-        print '---'
-        print unit['_source']
-        x=[]
+for unit in data_result['hits']['hits']:
+    if unit['_source'][label] != 'empty':
+        x = []
         for feature in unit['_source']:
             try:
-                if feature!=label:x.append(float(unit['_source'][feature]))
+                if feature != label:x.append(float(unit['_source'][feature]))
             except:
-                if feature!=label:x.append(float(vectors[feature].index(unit['_source'][feature])))
+                if feature != label:x.append(float(vectors[feature].index(unit['_source'][feature])))
         y.append(vectors[label].index(unit['_source'][label]))
         X.append(x)
-
-
-print X
-print '-----'
-print y
 
 # Train a LogisticRegression classifier
 clf = LogisticRegression(random_state = 0, solver='lbfgs', multi_class = 'multinomial').fit(X, y)
 
-
-# Train a GradientBoostingClassifier classifier
-#clf = ensemble.GradientBoostingClassifier()
-
-
 # Label the data units without human label
-URL = es_address + '/_search/'
+es_url = '{}/_search/'.format(es_address)
 query = {'query':{'bool': {'must': [{'match': {'es_id': es_id}}, {'match': {'es_id': es_id}}]}}, 'size': 10000}
-headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+response = requests.get(es_url, data = json.dumps(query), headers = HEADERS)
+data_result =  json.loads(response.text)
 
-response = requests.get(URL, data = json.dumps(query), headers = headers)
-result =  json.loads(response.text)
-
-for unit in result['hits']['hits']:
-    print unit['_source']['auto_label']
+for unit in data_result['hits']['hits']:
+    # Get the current data unit id
     doc_id = unit['_id']
-
-    URL = es_address + '/'+doc_id + '/_update'
-    print URL
-
+    es_url = '{}/{}/_update'.format(es_address, doc_id)
     current_features = []
-    for f in args.features.split(","):
+    for feature in args.features.split(','):
         try:
-            current_features.append(float(unit['_source'][f]))
+            current_features.append(float(unit['_source'][feature]))
         except:
             current_features.append(float(vectors[f].index(unit['_source'][f])))
-    print current_features
     current_proba = clf.predict_proba([current_features])[0]
     current_prediction = clf.predict([current_features])[0]
-
-    print current_prediction
-    print vectors[label]
-    print current_prediction
-    print vectors[label][current_prediction]
-    print current_proba
-    print '--------'
-    print clf.predict([current_features])
-    print clf.predict_proba([current_features])
-    print '--------'
     data = {'doc':{'auto_label':vectors[label][current_prediction],'auto_proba':current_proba[current_prediction]}}
-
     query = {'query':{'bool': {'must': [{'match': {'es_id': es_id}}, {'match': {'es_id': es_id}}]}}, 'size': 1000, '_source': source}
-
-
-    print requests.post(URL, data = json.dumps(data), headers = headers).text
+    try:
+        print(requests.post(es_url, data = json.dumps(data), headers = HEADERS).text)
+    except:
+        print("Something went wrong saving the label for {}".format(doc_id))
